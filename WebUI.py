eec82b6af7bbe64d3ae872de90213675cde28c53
31421 <1@1>
2024-05-26 23:08:16 +0800

    修复生成图片模糊

--- WebUI.py
+++ WebUI.py
@@ -7,29 +7,65 @@ import os
 import fawkes.protection as fp
 
 def process(input_image, level):
+    # 获取当前所处文件夹
+    cur_path = os.path.abspath(__file__)
+    cur_path = os.path.dirname(cur_path)
+    # 获取文件名
+    input_image_name = os.path.basename(input_image)
+    # 获取后缀
+    input_image_suffix = os.path.splitext(input_image_name)[1]
+
+    # 复制图像到imgs文件夹
+    copy_command = f'copy "{input_image}" "{cur_path}\\imgs"'
+    os.system(copy_command)
+
     # 获取当前时间
     now_time = time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())
-    input_img_name = f"./imgs/{now_time}_input.png"
-    output_img_name = input_img_name.replace("input", f"output_{level}")
-    # 保存图片
-    img = Image.fromarray(np.uint8(input_image))
-    img.save(input_img_name)
-
-    # try:
-    protector = fp.Fawkes("arcface_extractor_0", 0, 1, mode=level)
-
-    protector.run_protection([input_img_name], th=0.01, sd=1e6, lr=2,
-                            max_step=1000,
-                            batch_size=1, format="png",
-                            separate_target='store_true', debug='store_true', no_align='store_true')
-    # except Exception as e:
-    #     print(e)
-    #     print("Error")
-    #     return input_img_name
+    tmp_image_name = f"{cur_path}\\imgs\\{input_image_name.replace(input_image_suffix, f'{now_time}_input{input_image_suffix}')}"
+    output_img_name = f"{cur_path}\\imgs\\{input_image_name.replace(input_image_suffix, f'{now_time}_output{input_image_suffix}')}"
 
     # 重命名文件
-    os.rename(input_img_name.replace("input", f"input_cloaked"), 
+    os.rename(f"{cur_path}\\imgs\\{input_image_name}", tmp_image_name)
+
+    try:
+        import signal
+        signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+    except Exception as e:
+        pass
+
+    try:
+        protector = fp.Fawkes(
+            feature_extractor="arcface_extractor_0",
+            gpu="0",
+            batch_size=1,
+            mode=level
+        )
+
+        protector.run_protection(
+            [tmp_image_name],
+            th=0.01,
+            sd=1e6,
+            lr=2,
+            max_step=1000,
+            batch_size=1,
+            format=input_image_suffix[1:] if input_image_suffix[1:]!='jpg' else 'jpeg',
+            separate_target=False,
+            debug=False,
+            no_align=False
+        )
+
+
+    except Exception as e:
+        print(e)
+        print("Error")
+        # 删除临时文件
+        os.remove(tmp_image_name)
+        return input_image
+
+    # 重命名文件
+    os.rename(tmp_image_name.replace("input", f"input_cloaked"), 
               output_img_name)
+    os.remove(tmp_image_name)
 
     return output_img_name
 
@@ -38,7 +74,7 @@ def main():
         # 图片
         with gr.Row():
             # 上传图片
-            input_image = gr.Image(height=500, source='upload')
+            input_image = gr.Image(height=500, source='upload', type="filepath")
             # 输出图片
             ouput_image = gr.Image(height=500)
 
